95cf322801c72212a8a0e1152322686a6010aea7
DiegoDZ243 <s23120243@alumnos.itsur.edu.mx>
2025-11-27 20:59:45 -0600

    Add files via upload

---
+++ PuntoDeVentaPanaderia/PuntoDeVentaPanaderia/Pojos/clsAuditoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoDeVentaPanaderia.Pojos
+{
+    public class clsAuditoria
+    {
+        public string nombre {  get; set; }
+
+        public string cambio {  get; set; }
+        public string usuario { get; set; }
+        public decimal precioAnterior { get; set;  }
+        public decimal precioNuevo { get; set; }
+        public DateTime fecha { get; set; }
+
+        public clsAuditoria() { }
+    }
+}
