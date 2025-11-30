24fdbf78864b4dfcc9e88e5bde6dc4e1dd14bc47
Damian-18 <s23120229@alumnos.itsur.edu.mx>
2025-11-29 22:18:54 -0600

    Le quité el maximize nomas. jeje

--- PuntoDeVentaPanaderia/PuntoDeVentaPanaderia/Frontend/frmAgregarPan.Designer.cs
+++ PuntoDeVentaPanaderia/PuntoDeVentaPanaderia/Frontend/frmAgregarPan.Designer.cs
@@ -296,7 +296,6 @@
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmAgregarPan";
             this.Text = "frmAgregarPan";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmAgregarPan_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pctImagenPan)).EndInit();
             this.panel1.ResumeLayout(false);
